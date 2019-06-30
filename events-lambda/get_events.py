2e621ad11d99cb9bbfc1440f2842fd8c8139697d
swcollard <swcollard12@gmail.com>
2019-06-30 12:21:13 -0500

    Create a lambda function for fetching events by timestamp

---
+++ events-lambda/get_events.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import decimal
+from boto3.dynamodb.conditions import Key
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+# Fetch the table once the container spins up
+table = dynamodb.Table('change_management_events')
+
+def lambda_handler(event, context):
+    startTime = event.get('startTime')
+    endTime = event.get('endTime')
+    if not startTime or not endTime:
+        print event
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Bad Request. Missing a required parameter.")
+        }
+
+    response = table.scan(
+        FilterExpression=Key('timestamp').between(startTime, endTime)
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response['Items'], cls=DecimalEncoder)
+    }
